4d75b1134fc5ed7414ad5d02864e80ecbfc7a5ae
Eugene <korp@bk.ru>
2018-06-06 21:06:26 +0300

    Update findpn.py

--- findpn.py
+++ findpn.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
 '''
 The script for determining the part numbers of the disks and their number by the SPCollect dump
-Data of disks by their party numbers can be found in the document:
-https://www.emc.com/collateral/TechnicalDocument/docu31714.pdf
-
 To run the script you need to specify the path to the file SPA_cfg_info.txt which is in the archive * _SPA_datetime _ * _ sus.zip
 '''
 
 import re
+import requests
+
+def find_disk_info(part):
+    part = part.replace('PWR', '')
+    r = requests.get('http://www.harddrivesdirect.com/advanced_search_result_exact.php?keywords=' + part)
+    text = r.text
+    disk = re.search(r'<span class=roboto_blue12>(.*)<\/span>&nbsp;<span class=roboto_light_blue12>&nbsp;</span>', text)
+    if disk:
+        disk = disk.group(1).replace(part + ' ', '')
+        return disk
 
 filepath = 'SPA_cfg_info.txt'
 
@@ -32,6 +39,9 @@ for line in f:
                 count[pn] = 1
 
 print 'Total drives: ' + str(total)
+print '-' * 20
 for key, val in count.items():
+    disk = find_disk_info(key)
     print str(key) + ' - ' + str(val)
- 
+    print disk
+    print '-' * 20
